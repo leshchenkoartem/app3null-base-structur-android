a2d22ff2bfc8a3dbcc4a2619469b24bb40906316
misha <mishaakopov@gmail.com>
2021-08-06 12:00:52 +0400

    Added mising dependencies for Dagger Hilt

--- app/src/main/java/com/binding/mobile/App.kt
+++ app/src/main/java/com/binding/mobile/App.kt
@@ -1,11 +1,6 @@
 package com.binding.mobile
 
-import com.binding.mobile.diModules.localStorageModule
-import com.binding.mobile.diModules.repositoryModule
-import com.binding.mobile.diModules.useCasesModule
-import com.binding.mobile.diModules.viewModelsModule
 import com.app3null.basestructure.app.MvvmApplication
-import org.koin.core.module.Module
 
 class App: MvvmApplication() {
 }
